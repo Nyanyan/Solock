e010ca1b4a8ec473448b15baa370291ab97dc4d6
Nyanyan <nyanyama3141pi2@yahoo.co.jp>
2020-09-24 13:50:26 +0900

    solver written but slow :(

--- solver.py
+++ solver.py
@@ -49,16 +49,18 @@ def move(state, pins, direction, twist):
         res[i] %= 12
     return res
 
-scrambled_cube = [0, 1, 1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0] # UR1+
 solution = []
 
 def search(depth, state, banned_pins):
     global solution
+    if depth == 0:
+        return set(state) == {0}
     for direction in range(2): # 0: lower, 1: upper
         pin_candidate = range(1, 5) if direction else range(4)
         for num_of_pins in pin_candidate: # pins that are pulled
-            for pins in combinations(range(4), num_of_pins):
-                if set(pins) in banned_pins[direction]:
+            for pins_up in combinations(range(4), num_of_pins):
+                pins = [True if i in pins_up else False for i in range(4)]
+                if pins in banned_pins[direction]:
                     continue
                 move_clocks_time = [state[i] for i in y_move_clocks(pins, direction)]
                 not_move_clocks = n_move_clocks(pins, direction)
@@ -67,25 +69,33 @@ def search(depth, state, banned_pins):
                 else:
                     not_move_clocks -= set([1, 3, 4, 5, 7])
                 time_candidate = [state[i] for i in not_move_clocks]
-                twist_candidate = set([i - j for i in time_candidate for j in move_clocks_time])
+                twist_candidate = set([(i - j) % 12 for i in time_candidate for j in move_clocks_time])
                 for twist in twist_candidate:
-                    search()
-
-
-
+                    if twist == 0:
+                        continue
+                    n_state = move(state, pins, direction, twist)
+                    n_banned_pins = [[i for i in j] for j in banned_pins]
+                    n_banned_pins[direction].append(pins)
+                    solution.append([pins, direction, twist])
+                    if search(depth - 1, n_state, n_banned_pins):
+                        return True
+                    solution.pop()
+                    #n_banned_pins[direction].pop()
+    return False
 
 def solver(state):
     global solution
     for depth in range(13):
         solution = []
-        search(depth, state, [set([]), set([])])
-        judgenemt_state = [i for i in state]
-        for pins, direction, twist in solution:
-            judgenemt_state = move(judgenemt_state, pins, direction, twist)
-        if set(judgenemt_state) == {0}:
+        if search(depth, state, [[], []]):
             return solution
+        print(depth)
+    return -1
 
+#test_cube = [3, 6, 0, 3, 6, 3, 0, 0, 0, 9, 9, 9, 0, 0]
+test_cube = [5, 9, 6, 7, 9, 2, 8, 5, 1, 6, 9, 6, 0, 0] # UR1- DR2- DL5- UL0+ U5+ R5+ D5- L0+ ALL0+ y2 U6+ R3+ D0+ L6+ ALL3+
+print(test_cube)
 
-
+print(solver(test_cube))
 
 
