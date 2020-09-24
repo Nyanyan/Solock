0af768bb80b75a3c426500d85914f5ca406ea303
Nyanyan <nyanyama3141pi2@yahoo.co.jp>
2020-09-25 01:27:20 +0900

    main

--- main.py
+++ main.py
@@ -2,5 +2,10 @@ import tkinter
 
 from solver import solver
 from detector import detector
-from controller import controller
+#from controller import controller
 
+root = tkinter.Tk()
+root.title("Solock")
+root.geometry("320x240")
+
+root.mainloop()
