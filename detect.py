893ea2a161c57e8257471df12a368c99c40a5ec7
Nyanyan <nyanyama3141pi2@yahoo.co.jp>
2020-09-24 21:18:42 +0900

    detect

--- detect.py
+++ detect.py
@@ -25,16 +25,23 @@ def detect(direction):
                 value = [0 for _ in range(12)]
                 for tim in range(12):
                     deg = 30 * tim
-                    for r in range(radius // 3, radius // 2):
+                    for r in range(radius // 2, radius // 3 * 2):
                         x_circle = int(x + r * sin(deg * pi / 180))
                         y_circle = int(y - r * cos(deg * pi / 180))
-                        cv2.circle(gray, (x_circle, y_circle), 1, (tim * 255 // 12, 0, 0), thickness=1, lineType=cv2.LINE_8, shift=0)
-                        if dy == 0 and dx == 0:
-                            print(gray[x_circle, y_circle], end=' ')
-                        value[tim] += gray[x_circle, y_circle]
-                    if dy == 0 and dx == 0:
+                        '''
+                        if dy == 1 and dx == 1:
+                            print(gray[y_circle, x_circle], end=' ')
+                        '''
+                        value[tim] += gray[y_circle, x_circle]
+                    for r in range(radius // 2, radius // 3 * 2):
+                        x_circle = int(x + r * sin(deg * pi / 180))
+                        y_circle = int(y - r * cos(deg * pi / 180))
+                        cv2.circle(gray, (x_circle, y_circle), 1, (255, 255, 255), thickness=1, lineType=cv2.LINE_8, shift=0)
+                    '''
+                    if dy == 1 and dx == 1:
                         print('')
-                    value[tim] //= radius // 2 - radius // 3
+                    '''
+                    value[tim] //= radius // 3 * 2 - radius // 2
                 min_value_idx = value.index(min(value))
                 max_value_idx = value.index(max(value))
                 arr_out_min = [i for i in value]
@@ -46,17 +53,18 @@ def detect(direction):
                     clocks[coord] = max_value_idx
                 else:
                     clocks[coord] = min_value_idx
-                if dx == dy == 0:
-                    print(variance(arr_out_max), variance(arr_out_min), value)
         print(clocks)
-
-
-
-
-
+        for i, j in enumerate(clocks):
+            x = center_x + delta[i // 3] * dis
+            y = center_y + delta[i % 3] * dis
+            deg = 30 * j
+            x_circle = int(x + radius // 4 * 3 * sin(deg * pi / 180))
+            y_circle = int(y - radius // 4 * 3 * cos(deg * pi / 180))
+            cv2.circle(gray, (x_circle, y_circle), 3, (0, 0, 0), thickness=3, lineType=cv2.LINE_8, shift=0)
         cv2.imshow('title',gray)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
+    
 
     capture.release()
     cv2.destroyAllWindows()
